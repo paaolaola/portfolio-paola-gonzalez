baf991df0d775e14952cad67fc34284b756cc157
Paola Gonzalez <paolagonzalez.contacto@gmail.com>
2024-03-08 00:17:24 -0300

    last changes

--- src/views/FormContact.jsx
+++ src/views/FormContact.jsx
@@ -6,7 +6,7 @@ function FormContact() {
             <div className="box-form">
                 <h1 className="title-form">Contacto </h1>
                 <p className="text-form">
-                    Gracias por visitar mi portfolio! Estoy muy interesada en aprender sobre los proyectos e ideas de los demás. Por lo mismo, si estás
+                    Gracias por visitar mi portfolio! Estoy entusiasmada en aprender sobre los proyectos e ideas de los demás. Por lo mismo, si estás
                     interesado(a) en colaborar o si tienes preguntas, comentarios o simplemente quieres saludar, estaré atenta y con gusto te responderé.{" "}
                 </p>
                 <iframe id="typeform-full" src="https://6sujha3j6m7.typeform.com/to/cdgzp9LA?typeform-source=admin.typeform.com"></iframe>{" "}
